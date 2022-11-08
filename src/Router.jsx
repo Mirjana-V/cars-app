bffb0bfa74d9395016c69089e117ed759c115eaa
Mirjana-V <tmirjana7@gmail.com>
2022-11-08 20:02:27 +0100

    Zadatak 1

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+import AppCars from './pages/AppCars';
+
+
+
+export default function Router() {
+  return (
+    <Switch>
+      <Route path="/cars">
+        <AppCars />
+      </Route>
+    </Switch>
+  );
+}
